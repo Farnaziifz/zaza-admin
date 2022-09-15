4d1f044200761c21b9d1b3bf2d78d338f3600bf7
Behniya <behniya.azad@webjar.ir>
2022-09-15 11:42:52 +0430

    feat BOOMS-115: Add Hint collapse to page

--- src/core/types/hints.type.ts
+++ src/core/types/hints.type.ts
@@ -1,4 +1,3 @@
 export type hintType = {
-  body? : string
+  body?: string
 }
-
--- src/presentation/pages/LabelSettingsLabels.vue
+++ src/presentation/pages/LabelSettingsLabels.vue
@@ -1,12 +1,23 @@
 <script setup lang="ts">
 import ContentLayout from '../layouts/ContentLayout.vue'
+import HintCollapse from '../components/shared/Organisms/HintCollapse.vue'
+import { t } from 'vui18n'
+import { hintType } from '../../core/types/hints.type'
+
+const hints: hintType[] = [
+  {
+    body: t('pages.LabelSettingsLabels.hint'),
+  },
+]
 </script>
 
 <template>
   <content-layout>
-    <template #content-title> برچسب </template>
+    <template #content-title> برچسب</template>
 
-    <template #content-body> </template>
+    <template #content-body>
+      <hint-collapse :hints="hints"></hint-collapse>
+    </template>
   </content-layout>
 </template>
 

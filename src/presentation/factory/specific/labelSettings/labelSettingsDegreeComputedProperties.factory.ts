112a18ebf658c03ac473e9de442236413e921b39
Behniya <behniya.azad@webjar.ir>
2022-09-19 11:26:52 +0430

    feat BOOMS-189: change Api from score to degree (fixed).

--- src/presentation/factory/specific/labelSettings/labelSettingsDegreeComputedProperties.factory.ts
+++ src/presentation/factory/specific/labelSettings/labelSettingsDegreeComputedProperties.factory.ts
@@ -1,21 +1,21 @@
 import { computed, Ref } from 'vue'
-import { scoreType } from '../../../../core/enums/scoreType.enum'
-import { score } from '../../../../core/types/score.type'
+import { degreeType } from '../../../../core/enums/degreeType.enum'
+import { degree } from '../../../../core/types/degree.type'
 
 export const computedPropertiesFactory = (
   value: Ref<string | null>,
-  score: Ref<score>
+  degree: Ref<degree>
 ) => {
   const isSubmitButtonDisabledComputedFactory = () =>
     computed(() => {
-      if (!score.value.isActive) return true
-
       if (value.value === '') return true
 
-      if (score.value.amount === 0) return true
+      if (degree.value.amount === 0) return true
 
-      if (value.value === scoreType.ORDER && !score.value.value) return true
-      else if (value.value === scoreType.PRICE && !score.value.value) return true
+      if (value.value === degreeType.PER_ORDER && !degree.value.value)
+        return true
+      else if (value.value === degreeType.PER_PRICE && !degree.value.value)
+        return true
 
       return false
     })

c12b895ac23dd88b65acb00da8322efe5da23c85
farnazii <ffarzipour92@gmail.com>
2022-09-20 14:55:37 +0430

    feat BOOMS-47: add incentive details layouts

--- components.d.ts
+++ components.d.ts
@@ -7,6 +7,8 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
+    ABreadcrumb: typeof import('ant-design-vue/es')['Breadcrumb']
+    ABreadcrumbItem: typeof import('ant-design-vue/es')['BreadcrumbItem']
     AButton: typeof import('ant-design-vue/es')['Button']
     ACard: typeof import('ant-design-vue/es')['Card']
     ACollapse: typeof import('ant-design-vue/es')['Collapse']

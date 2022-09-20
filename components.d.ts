9462352bec9b7b0505f0f93844da49bde387a0f1
Behniya <behniya.azad@webjar.ir>
2022-09-20 16:40:34 +0430

    feat BOOMS-196: add font to the project.

--- components.d.ts
+++ components.d.ts
@@ -7,8 +7,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    ABreadcrumb: typeof import('ant-design-vue/es')['Breadcrumb']
-    ABreadcrumbItem: typeof import('ant-design-vue/es')['BreadcrumbItem']
     AButton: typeof import('ant-design-vue/es')['Button']
     ACard: typeof import('ant-design-vue/es')['Card']
     ACollapse: typeof import('ant-design-vue/es')['Collapse']

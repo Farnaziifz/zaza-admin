6fb95062b78cc36857f439d33998b749d1820fe4
farnazii <ffarzipour92@gmail.com>
2022-10-04 16:02:51 +0330

    feat BOOMS-125: add empty layout

--- src/presentation/components/specific/Customer/CustomerMembershipGroup.vue
+++ src/presentation/components/specific/Customer/CustomerMembershipGroup.vue
@@ -1,16 +1,16 @@
 <script lang="ts" setup>
+import EmptyLayout from '/src/presentation/layouts/EmptyLayout.vue'
 type CustomerMembershipGroupPropsType = {
   groupData: { title: string }[]
 }
-const props = defineProps < CustomerMembershipGroupPropsType>()
-
+const props = defineProps<CustomerMembershipGroupPropsType>()
 </script>
 <template>
   <a-card
     :bodyStyle="{ 'box-shadow': '0px 4px 10px rgba(0, 0, 0, 0.1)' }"
     :bordered="false"
     class="info-card"
-    v-if="props.groupData && props.groupData.length"
+    v-if="!props.groupData && !props.groupData.length"
   >
     <a-typography-title :level="4">دسته بندی های عضو</a-typography-title>
     <div class="key">نام دسته بندی‌ها</div>
@@ -20,6 +20,9 @@ const props = defineProps < CustomerMembershipGroupPropsType>()
       </li>
     </ul>
   </a-card>
+  <EmptyLayout v-else>
+    <template #empty-text> دسته بندی عضو یافت نشد </template>
+  </EmptyLayout>
 </template>
 
 <style lang="scss" scoped>

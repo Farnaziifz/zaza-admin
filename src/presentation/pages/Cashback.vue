fa7c5fda8ff7df6c1e45f246eb0cc89a92fb5d28
Behniya <behniya.azad@webjar.ir>
2022-10-09 17:51:35 +0330

    feat BOOMS-39: Cashback page initialized.

---
+++ src/presentation/pages/Cashback.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import { ref } from 'vue'
+
+const serverData = ref()
+</script>
+
+<template>
+  <div>
+    <a-card style="margin: 8px 0; background-color: #f6f6f6">
+      <div class="flex justify-between">
+        <h2>فعال سازی کش‌بک</h2>
+        <a-switch :checked-value="serverData.isActive" />
+        <!--            @click="openSubmissionModal"-->
+      </div>
+    </a-card>
+  </div>
+</template>

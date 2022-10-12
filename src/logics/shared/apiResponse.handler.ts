f3fcda2bd580c9ab69fc1801faa4f661e408f826
Behniya <b3hniya@gmail.com>
2022-10-12 08:48:31 +0330

    feat BOOMS-39: prettify all files

--- src/logics/shared/apiResponse.handler.ts
+++ src/logics/shared/apiResponse.handler.ts
@@ -1,52 +1,49 @@
-import {AxiosError, AxiosResponse} from 'axios'
-import {showErrorMessage} from '@/logics/shared/message.handler'
+import { AxiosError, AxiosResponse } from 'axios'
+import { showErrorMessage } from '@/logics/shared/message.handler'
 
 type response<T = never> = {
-    data?: T
-    errors?: {
-        message: string
-    }
+  data?: T
+  errors?: {
+    message: string
+  }
 }
 
 const defaultError: response = {
-    errors: {
-        message: 'ناموفق'
-    }
+  errors: {
+    message: 'ناموفق',
+  },
 }
 
 export const makeARequest = async <T>(
-    apiHandler:
-        | (<T>(route: string, data?: T) => Promise<AxiosResponse<response<T>>>)
-        | (<T>(route: string) => Promise<AxiosResponse<response<T>>>),
-    route: string,
-    data?: T
+  apiHandler:
+    | (<T>(route: string, data?: T) => Promise<AxiosResponse<response<T>>>)
+    | (<T>(route: string) => Promise<AxiosResponse<response<T>>>),
+  route: string,
+  data?: T
 ): Promise<response<T>> => {
-    try {
-        if (data) return apiHandler(route, data).then(res => res.data)
-        else return apiHandler(route).then(res => res.data) as response<T>
-    } catch (e) {
-        const err = e as AxiosError<response<T>>
-        return handleError<T>(err)
-    }
+  try {
+    if (data) return apiHandler(route, data).then((res) => res.data)
+    else return apiHandler(route).then((res) => res.data) as response<T>
+  } catch (e) {
+    const err = e as AxiosError<response<T>>
+    return handleError<T>(err)
+  }
 }
 
 export const handleError = <T>(e: AxiosError<response<T>>): response<T> => {
-    handleErrorMessageBroadcasting<T>(e)
+  handleErrorMessageBroadcasting<T>(e)
 
-    if (e.response)
-        return e.response.data
-    else
-        return defaultError
+  if (e.response) return e.response.data
+  else return defaultError
 }
 
 export const handleErrorMessageBroadcasting = <T>(
-    e: AxiosError<response<T>>
+  e: AxiosError<response<T>>
 ) => {
-    if (e.response?.data.errors) {
-        showErrorMessage(e.response.data.errors.message)
-    } else {
-        showErrorMessage(e.message)
-    }
-    return e
+  if (e.response?.data.errors) {
+    showErrorMessage(e.response.data.errors.message)
+  } else {
+    showErrorMessage(e.message)
+  }
+  return e
 }
-
--- src/logics/specific/cashback.handler.ts
+++ src/logics/specific/cashback.handler.ts
@@ -1,26 +1,26 @@
-import {cashbackApi} from '@/resources/api/cashback'
-import {cashback} from "@/core/types/cashback.type";
+import { cashbackApi } from '@/resources/api/cashback'
+import { cashback } from '@/core/types/cashback.type'
 import {
-    showErrorMessage,
-    showLoadingMessage,
-    showSuccessMessage,
+  showErrorMessage,
+  showLoadingMessage,
+  showSuccessMessage,
 } from '@/logics/shared/message.handler'
 
 const api = cashbackApi()
 
 export const initHandler = async () => {
-    return await api.get()
+  return await api.get()
 }
 
 export const changeStatusHandler = async () => {
-    showLoadingMessage()
-    const res = await api.putStatus()
-    if (res.status === 200 || res.status === 204) showSuccessMessage()
-    else showErrorMessage()
+  showLoadingMessage()
+  const res = await api.putStatus()
+  if (res.status === 200 || res.status === 204) showSuccessMessage()
+  else showErrorMessage()
 }
 
 export const sendCashbackDataToServer = async (cashbackData: cashback) => {
-    showLoadingMessage()
-    const res = await api.put(cashbackData)
-    if (!res.errors) showSuccessMessage()
+  showLoadingMessage()
+  const res = await api.put(cashbackData)
+  if (!res.errors) showSuccessMessage()
 }

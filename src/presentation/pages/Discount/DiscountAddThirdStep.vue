e48b99e33669e65d0c0cf4fdbd86f22136d26bf8
farnazii <ffarzipour92@gmail.com>
2022-10-20 19:20:15 +0330

    feat BOOMS-179: add thirs step router resource

---
+++ src/presentation/pages/Discount/DiscountAddThirdStep.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+import ContentLayout from '/src/presentation/layouts/ContentLayout.vue'
+import HintCollapse from '/src/presentation/components/shared/Organisms/HintCollapse.vue'
+import BSelect from '/src/presentation/components/shared/atoms/BSelect.vue'
+import { ref } from 'vue'
+import { t } from 'vui18n'
+
+const selectedCategory = ref('')
+const categoryOptions: any[] = []
+</script>
+<template>
+  <content-layout>
+    <template #content-body>
+      <hint-collapse
+        :hints="[
+          {
+            body: t('pages.DiscountAdd.hints.hintBody'),
+            description: t('pages.DiscountAdd.hints.hintDescription'),
+          },
+        ]"
+      />
+      <div class="form-container mt-4">
+        <div>دسته‌بندی</div>
+        <BSelect
+          v-model:value="selectedCategory"
+          placeholder="دسته‌بندی مورد نظر را انتخاب کنید"
+          :options="categoryOptions"
+        />
+      </div>
+    </template>
+  </content-layout>
+</template>

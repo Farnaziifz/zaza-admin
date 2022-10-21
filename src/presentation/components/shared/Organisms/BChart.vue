6fe1d92cb9e599715c3d12fcf2057d22fe0d25e6
Behniya <b3hniya@gmail.com>
2022-10-21 21:45:19 +0330

    feat BOOMS-152: add b-chart component to the application.

---
+++ src/presentation/components/shared/Organisms/BChart.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+import { Bar, Pie, Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+} from 'chart.js'
+import { computed } from 'vue'
+import { chartProps } from '@/core/types/chart.type'
+
+ChartJS.register(
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement
+)
+
+const charts = {
+  Bar,
+  Pie,
+  Line,
+}
+const props = defineProps<chartProps>()
+const chartType = computed(() => charts[props.chartType])
+</script>
+
+<template>
+  <component
+    :is="chartType"
+    :chart-options="props.chartOptions"
+    :chart-data="props.chartData"
+  ></component>
+</template>

e9bf3a3fba7b6baf4eaa47da467b265de217c6e5
Behniya <behniya.azad@webjar.ir>
2022-10-24 10:39:56 +0330

    feat BOOMS-132: add chart base to the application.

--- src/presentation/pages/Report/ReportDiscountList.vue
+++ src/presentation/pages/Report/ReportDiscountList.vue
@@ -1,27 +1,44 @@
 <script lang="ts" setup>
 import ContentLayout from '@/presentation/layouts/ContentLayout.vue'
 import BChart from '@/presentation/components/shared/Organisms/BChart.vue'
+import _ from 'lodash'
 
+//BOOMS 132 Start
 const chartData = {
-  labels: ['daskldj', 'dasl', 'dsa', '123'],
+  labels: ['daskldj', 'dasl', 'dsa', '123', 'test', 'test1', 'test2'],
   datasets: [
     {
       label: 'Fully Rounded',
-      data: [1, 2, 3, 4],
+      data: [1, 2, 3, 4, 5, 6, 7],
       borderWidth: 2,
       backgroundColor: '#434348',
     },
     {
       label: 'Small Radius',
-      data: [5, 6, 7, 8],
+      data: [5, 6, 7, 8, 9, 10, 11],
       borderWidth: 2,
       backgroundColor: '#7CB5EC',
     },
   ],
 }
+
+const chartOptions = {
+  responsive: true,
+  scales: {
+    y: {
+      ticks: {
+        callback: (value: number | string, index: number) => {
+          return _.toString(index) + ' dkjashd'
+        },
+      },
+    },
+  },
+}
+//BOOMS 132 End
 </script>
 <template>
   <ContentLayout>
+    <!--  BOOMS-132 Start -->
     <template #content-title>
       <span style="font-weight: 700; font-size: 24px"> گزارش کد تخفیف </span>
     </template>
@@ -35,11 +52,12 @@ const chartData = {
         <BChart
           chart-type="Bar"
           :chart-data="chartData"
-          :chart-options="{ responsive: true }"
+          :chart-options="chartOptions"
           :height="484"
           :width="1184"
-        ></BChart>
+        />
       </a-card>
+      <!--  BOOMS-132 End -->
     </template>
   </ContentLayout>
 </template>

966dd239e1f771b336f0b5f1180427c6afc1c4f5
Behniya <behniya.azad@webjar.ir>
2022-10-24 11:12:16 +0330

    feat BOOMS-133: lint files.

--- src/core/types/discounts.type.ts
+++ src/core/types/discounts.type.ts
@@ -1,77 +1,76 @@
 // import { CouponsTypesType, CouponsRewardsType } from '../enums/couponsType.enum'
-import {pagination} from "@/core/types/pagination.type";
+import { pagination } from '@/core/types/pagination.type'
 
 export type discountReport = {
-    title?: string
-    code?: string
-    successRate?: number
-    cost?: number
-    income?: number
-    customerCount?: number
-    groupsTitle?: string[]
-    groupsId?: string[]
+  title?: string
+  code?: string
+  successRate?: number
+  cost?: number
+  income?: number
+  customerCount?: number
+  groupsTitle?: string[]
+  groupsId?: string[]
 }
 
 export type promotionUsageReport = {
-    items: discountReport[]
+  items: discountReport[]
 } & pagination
 
-
 export type group = {
-    groupId: string
-    title: string
-    customersCount: number
+  groupId: string
+  title: string
+  customersCount: number
 }
 type promotionStep = {
-    order?: number
-    amount?: number
+  order?: number
+  amount?: number
 }
 export type discounts = {
-    id: string
-    title: string
-    type: string
-    consumeType: string
-    stateType: string
-    amount: number
-    code?: string
-    consumeLimitation?: number
-    startAt: string
-    expireAt: string
-    notificationType?: string[]
-    customersCount: number
-    promotionAssignedGroups?: group[]
-    maximumAmount?: number
-    promotionSteps?: promotionStep[]
-    isActive: boolean
+  id: string
+  title: string
+  type: string
+  consumeType: string
+  stateType: string
+  amount: number
+  code?: string
+  consumeLimitation?: number
+  startAt: string
+  expireAt: string
+  notificationType?: string[]
+  customersCount: number
+  promotionAssignedGroups?: group[]
+  maximumAmount?: number
+  promotionSteps?: promotionStep[]
+  isActive: boolean
 }
 
 export type discountsList = {
-    items: discounts[]
-    hasNextPage: boolean
-    hasPreviousPage: boolean
-    page: number
-    totalCount: number
-    totalPages: number
+  items: discounts[]
+  hasNextPage: boolean
+  hasPreviousPage: boolean
+  page: number
+  totalCount: number
+  totalPages: number
 }
 
 export type discountGroup = {
-    items: group[]
-    hasNextPage: boolean
-    hasPreviousPage: boolean
-    page: number
-    totalCount: number
-    totalPages: number
+  items: group[]
+  hasNextPage: boolean
+  hasPreviousPage: boolean
+  page: number
+  totalCount: number
+  totalPages: number
 }
 
 export type discountCustomer = {
-    fullName: string
-    mobileNumber: number
+  fullName: string
+  mobileNumber: number
 }
 export type discountCustomerGroup = {
-    items: discountCustomer[]
-    hasNextPage: boolean
-    hasPreviousPage: boolean
-    page: number
-    totalCount: number
-    totalPages: number
+  items: discountCustomer[]
+  hasNextPage: boolean
+  hasPreviousPage: boolean
+  page: number
+  totalCount: number
+  totalPages: number
 }

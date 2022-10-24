b38ef7078a07cca0c22fccbca5d6de42a8aa8be6
farnaziii <ffarzipour92@gmail.com>
2022-10-24 04:27:04 -0400

    feat BOOMS-180: add fourth step

--- src/resources/router/DiscoutRouter.config.ts
+++ src/resources/router/DiscoutRouter.config.ts
@@ -3,6 +3,7 @@ import DiscountList from '/src/presentation/pages/Discount/DiscountList.vue'
 import DiscountDetails from '/src/presentation/pages/Discount/DiscountDetails.vue'
 import DiscountAdd from '/src/presentation/pages/Discount/DiscountAdd.vue'
 import DiscountAddFirstStep from '/src/presentation/pages/Discount/DiscountAddFirstStep.vue'
+import DiscountFourthStep from '/src/presentation/pages/Discount/DiscountAddFourthStep.vue'
 import DiscountAddSecondStep from '/src/presentation/pages/Discount/DiscountAddSecoundStep.vue'
 import DiscountAddThirdStep from '/src/presentation/pages/Discount/DiscountAddThirdStep.vue'
 
@@ -33,6 +34,12 @@ export const discountRouterConfig = {
           name: 'discount-add-first-step',
           meta: { step: 0 },
           component: DiscountAddFirstStep,
+        },
+        {
+          path: 'fourth-step',
+          name: 'discount-add-fourth-step',
+          meta: { step: 3 },
+          component: DiscountFourthStep,
         },
         {
           path: 'second-step',

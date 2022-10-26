f6fd6ea3a303319f434698db906f3d85bdc21ca5
Behniya <b3hniya@gmail.com>
2022-10-26 07:46:03 +0330

    feat BOOMS-182: add rfm component to the application.

---
+++ src/presentation/components/shared/Organisms/BRfmChart.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+<template>
+  <div class="flex flex-row">
+    <div class="flex flex-col">
+      <span></span>
+      <div class="flex flex-row">
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <span></span>
+      <div class="flex flex-row">
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  </div>
+</template>

2a0fd9f9366a71c54edb9d8877d655005c1e5c44
Behniya <b3hniya@gmail.com>
2022-10-26 07:16:18 +0330

    feat BOOMS-140: add chart to the coupon usage page.

---
+++ src/presentation/pages/Report/ReportCouponList.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import ContentLayout from '@/presentation/layouts/ContentLayout.vue'
+</script>
+
+<template>
+  <content-layout>
+    <template #content-title> گزارش کوپن‌ها </template>
+    <template #content-body> </template>
+  </content-layout>
+</template>

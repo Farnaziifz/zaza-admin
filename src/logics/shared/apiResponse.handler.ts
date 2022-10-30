151cf38c67caa6a2e9df3caad01b75c1b5ff8735
Behniya <behniya.azad@webjar.ir>
2022-10-30 11:21:00 +0330

    feat BOOMS-184: fix api call for detail cards

--- src/logics/shared/apiResponse.handler.ts
+++ src/logics/shared/apiResponse.handler.ts
@@ -4,7 +4,7 @@ import {
   showLoadingMessage,
   showSuccessMessage,
 } from '@/logics/shared/message.handler'
-import {api} from "@/resources/api";
+import { api } from '@/resources/api'
 
 type response<T = never> = {
   data?: T
@@ -20,10 +20,8 @@ export const makeARequest = async <T>(
   route: string,
   data?: T
 ): Promise<response<T>> => {
-  if(apiHandler === api.get)
-    showLoadingMessage('درحال دریافت دیتا')
-  else
-    showLoadingMessage()
+  if (apiHandler === api.get) showLoadingMessage('درحال دریافت دیتا')
+  else showLoadingMessage()
   try {
     const res = await apiHandler(route, data)
     handleSuccessMessageBroadcasting(res.status)

27ed6ab2556dff890c911d54d3123c4eb7afb031
Behniya <behniya.azad@webjar.ir>
2022-10-30 10:50:21 +0330

    feat BOOMS-184: add detail cards, add radio btn, add title

---
+++ src/core/constants/dashboard.options.ts
@@ -0,0 +1,65 @@
+export const degreeChartOptions = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      text: 'ارزیابی جایگاه مشتریان',
+      align: 'end',
+      padding: {
+        bottom: 24,
+      },
+      font: {
+        size: 16,
+        family: 'YekanBakh',
+        weight: 500,
+      },
+    },
+    legend: {
+      display: true,
+      position: 'right',
+      labels: {
+        pointStyle: 'circle',
+        usePointStyle: true,
+        textAlign: 'right',
+        font: {
+          size: 14,
+          family: 'YekanBakh',
+          weight: 500,
+        },
+      },
+    },
+  },
+}
+
+export const orderChartOptions = {
+  responsive: true,
+  plugins: {
+    title: {
+      display: true,
+      text: 'ارزیابی رضایتمندی مشتریان',
+      align: 'end',
+      padding: {
+        bottom: 24,
+      },
+      font: {
+        size: 16,
+        family: 'YekanBakh',
+        weight: 500,
+      },
+    },
+    legend: {
+      display: true,
+      position: 'right',
+      labels: {
+        pointStyle: 'circle',
+        usePointStyle: true,
+        textAlign: 'right',
+        font: {
+          size: 14,
+          family: 'YekanBakh',
+          weight: 500,
+        },
+      },
+    },
+  },
+}

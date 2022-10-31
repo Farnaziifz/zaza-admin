75988a1327a212c7afe344a965a682eb342f83e3
farnaziii <ffarzipour92@gmail.com>
2022-10-31 03:54:59 -0400

    feat BOOMS-146: add group details router

---
+++ src/presentation/pages/group/GroupDetails.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import IncentiveDetailLayout from '/src/presentation/layouts/IncentiveDetailLayout.vue'
+</script>
+
+<template>
+  <IncentiveDetailLayout>
+    <template #layout-title>جزئیات دسته‌بندی مشتریان جدید</template>
+    <template #layout-actions>
+      <div class="actions-button">
+        <a-tag color="green">فعال</a-tag>
+        <a-tag color="red">غیرفعال</a-tag>
+        <a-button type="primary" size="small">تغییر وضعیت</a-button>
+      </div>
+    </template>
+    <template #layout-content> </template>
+  </IncentiveDetailLayout>
+</template>

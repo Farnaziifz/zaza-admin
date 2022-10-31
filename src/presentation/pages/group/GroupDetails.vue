a5e7d91799697287a0a31d1c6137e081d9b0d2ba
farnaziii <ffarzipour92@gmail.com>
2022-10-31 07:40:45 -0400

    BOOMS-146: add card collapse component

--- src/presentation/pages/group/GroupDetails.vue
+++ src/presentation/pages/group/GroupDetails.vue
@@ -1,5 +1,6 @@
 <script lang="ts" setup>
 import IncentiveDetailLayout from '/src/presentation/layouts/IncentiveDetailLayout.vue'
+import CardCollapse from '/src/presentation/components/shared/Organisms/CardCollapse.vue'
 </script>
 
 <template>
@@ -12,6 +13,10 @@ import IncentiveDetailLayout from '/src/presentation/layouts/IncentiveDetailLayo
         <a-button type="primary" size="small">تغییر وضعیت</a-button>
       </div>
     </template>
-    <template #layout-content> </template>
+    <template #layout-content>
+      <CardCollapse title="مشوق های وابسته">
+        <template #body></template>
+      </CardCollapse>
+    </template>
   </IncentiveDetailLayout>
 </template>

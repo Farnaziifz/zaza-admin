af9f17bebb44c3a412eb557f509c957f825d01b9
Behniya <behniya.azad@webjar.ir>
2022-10-31 12:46:02 +0330

    feat BOOMS-182: fix rfm chart.

--- src/presentation/pages/Main.vue
+++ src/presentation/pages/Main.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import MainLayout from '../layouts/MainLayout.vue'
 import BMessage from '@/presentation/components/shared/atoms/BMessage.vue'
-import BRfmChart from '@/presentation/components/shared/Organisms/BRfmChart.vue'
 
 import { useMainStore } from '@/resources/store/main.store'
 
@@ -13,16 +12,6 @@ const closeMessage = () => mainStore.closeMessage()
   <b-message v-bind="mainStore.message" @on-close="closeMessage" />
   <MainLayout>
     <template #pageContent>
-      <BRfmChart
-        class="m-16"
-        :hero-amount="3"
-        :loyal-amount="1"
-        :attention-need-amount="2"
-        :lazy-amount="6"
-        :churn-amount="3"
-        :normal-amount="1"
-      />
-
       <router-view />
     </template>
   </MainLayout>

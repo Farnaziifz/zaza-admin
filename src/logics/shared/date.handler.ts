acab503befb438cc5a32d3f52c4cb8df50cfcab7
Behniya <behniya.azad@webjar.ir>
2022-10-31 10:10:38 +0330

    feat BOOMS-184: refactor code

--- src/logics/shared/date.handler.ts
+++ src/logics/shared/date.handler.ts
@@ -2,8 +2,58 @@
 // @ts-ignore
 import pd from 'persian-date'
 import _ from 'lodash'
+import {reportPeriodType} from "@/core/enums/reportType.enum";
+import momentJalali, {unitOfTime} from 'jalali-moment'
+import moment from 'moment'
+
+
+export const getAllDaysInPeriodQuery = (type: reportPeriodType) => {
+    let momentStartOfType: unitOfTime.StartOf = 'weeks'
+    let step: unitOfTime.DurationConstructor = 'jD'
+    let diffUnit: unitOfTime.Diff = 'days'
+    switch (type) {
+        case 'WEEKLY':
+            momentStartOfType = 'jw'
+            break
+        case 'MONTHLY':
+            momentStartOfType = 'jMonth'
+            break
+        case 'ANNUAL':
+            momentStartOfType = 'jYear'
+            step = 'jMonth'
+            diffUnit = 'month'
+            break
+    }
+
+    const format = 'yyyy/MM/DD-hh:mm:ss'
+    const startOfRange = momentJalali().utc().startOf(momentStartOfType)
+    const today = momentJalali().utc().startOf(step)
+    const startOfRangeDiffUntilNow = today.diff(startOfRange, diffUnit)
+
+    const range: { from: Date; to: Date }[] = []
+    const currentDate = startOfRange.clone()
+
+    //TODO MAHDI
+    for (let i = 0; i <= startOfRangeDiffUntilNow; i++) {
+        range.push({
+            from: currentDate.startOf(step).startOf('jDay').toDate(),
+            to: currentDate.endOf(step).startOf('jDay').toDate(),
+        })
+        currentDate.add(1, step)
+    }
+
+
+    let q = `?Format=${format}&`
+    range.forEach((el, index) => {
+        q += `Ranges[${index}].From=${moment(el.from).format(
+            format
+        )}&Ranges[${index}].To=${moment(el.to).format(format)}&`
+    })
+    return q
+}
+
 
 export const convertDateFromPersianToGeorgian = (
-  date: string
+    date: string
 ): Date | undefined =>
-  new pd(date.split('/').map((x: string) => _.toNumber(x))).toDate()
+    new pd(date.split('/').map((x: string) => _.toNumber(x))).toDate()
--- src/resources/api/report.ts
+++ src/resources/api/report.ts
@@ -2,12 +2,17 @@ import { makeARequest } from '@/logics/shared/apiResponse.handler'
 import { api } from '@/resources/api/index'
 import { promotionUsageReport } from '@/core/types/discounts.type'
 import { couponsList } from '@/core/types/coupons.type'
+import { reportPeriodType } from '@/core/enums/reportType.enum'
+import { getAllDaysInPeriodQuery } from '@/logics/shared/date.handler'
 import { cashbackStatics } from '../../core/types/cashback.type'
+
 const baseUrl = 'report'
 const promotionUsage = '/promotion-usage'
 const couponUsage = '/coupon-usage'
 const dashboardStatisticsOverall = '/dashboard-statistics-overall'
 const creditFinancialEvaluation = '/credit-financial-evaluation'
+const cashbackFinancialEvaluation = '/cashback-financial-evaluation'
+const promotionFinancialEvaluation = '/promotion-financial-evaluation'
 
 export const getDashboardStatisticsOverall = async () =>
   await makeARequest<{
@@ -34,9 +39,43 @@ const getReportCachbackStatistic = async () =>
     `${baseUrl}/cashback-statistics-overall`
   )
 
+const getCreditFinancialEvaluation = async (type: reportPeriodType) => {
+  const query = getAllDaysInPeriodQuery(type)
+  const res = await makeARequest(
+      api.get,
+      `${baseUrl}${creditFinancialEvaluation}${query}`
+  )
+
+  return res
+}
+
+
+const getCashbackFinancialEvaluation = async (type: reportPeriodType) => {
+  const query = getAllDaysInPeriodQuery(type)
+  const res = await makeARequest(
+      api.get,
+      `${baseUrl}${cashbackFinancialEvaluation}${query}`
+  )
+
+  return res
+}
+
+const getPromotionFinancialEvaluation = async (type: reportPeriodType) => {
+  const query = getAllDaysInPeriodQuery(type)
+  const res = await makeARequest(
+      api.get,
+      `${baseUrl}${promotionFinancialEvaluation}${query}`
+  )
+
+  return res
+}
+
 export const reportApi = () => ({
   getReportPromotionUsage,
   getReportCouponUsage,
   getReportCachbackStatistic,
   getDashboardStatisticsOverall,
+  getCreditFinancialEvaluation,
+  getCashbackFinancialEvaluation,
+  getPromotionFinancialEvaluation,
 })

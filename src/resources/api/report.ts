6236346fc8a424ec710a56d59be644bd589962e4
Behniya <behniya.azad@webjar.ir>
2022-11-01 15:15:09 +0330

    feat BOOMS-184: lint and sync with dev.

--- src/resources/api/report.ts
+++ src/resources/api/report.ts
@@ -6,6 +6,15 @@ import { cashbackStatics } from '../../core/types/cashback.type'
 const baseUrl = 'report'
 const promotionUsage = '/promotion-usage'
 const couponUsage = '/coupon-usage'
+const dashboardStatisticsOverall = '/dashboard-statistics-overall'
+
+export const getDashboardStatisticsOverall = async () =>
+  await makeARequest<{
+    numberOfCustomers: number
+    numberOfFeedbacks: number
+    customerWithRepurchase: number
+  }>(api.get, `${baseUrl}${dashboardStatisticsOverall}`)
+
 export const getReportPromotionUsage = async (page = 1) =>
   await makeARequest<promotionUsageReport>(
     api.get,
@@ -28,4 +37,5 @@ export const reportApi = () => ({
   getReportPromotionUsage,
   getReportCouponUsage,
   getReportCachbackStatistic,
+  getDashboardStatisticsOverall,
 })
